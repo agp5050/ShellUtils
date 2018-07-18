9f043294a00fa309df6046c4bba0373fc27e5f84
philip <agp5050554@163.com>
2018-07-18 17:57:27 +0800

    Auto Scp2Slaves Files

---
+++ AutoScp2Slaves.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+echo $TOMCAT_HOME
+if [ $? -eq 0 ]; then
+    echo 'TOMCAT_HOME set well.'
+else
+    echo 'TOMCAT_HOME not set, must be set'
+fi
+if [ $# -ne 1 ]; then
+    echo 'shell args not enough,must be 1'
+    exit 1
+fi
+filename=$1
+if [ -f "${TOMCAT_HOME}/webapps/ROOT/WEB-INF/slaves" ];then
+  tables=`cat $TOMCAT_HOME/webapps/ROOT/WEB-INF/slaves`
+  if [ -z "${tables}" ];then
+     echo "从文件:slaves  文件为空!!!"
+     exit
+  fi
+  for table in $tables;
+  do
+    table=`echo $table|grep ^[^#]` ##过滤以＃开头的字符串
+    echo "###正在向${table}同步${filename}..."
+    folder=`echo ${filename%/*}` #切割出来文件夹目录，如果文件夹不存在则创建
+    ssh $table "mkdir -p $folder;exit"
+    echo "mkdirs"
+    scp $filename $table:$folder
+    echo "Done"
+  done
+else
+  echo "从文件:slaves  不存在!!!"
+  exit
+fi
